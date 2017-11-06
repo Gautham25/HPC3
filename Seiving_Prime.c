b7424ee912a31c11b455e19d936acb3b617fa2cc
Gautham25 <32765396+Gautham25@users.noreply.github.com>
2017-11-06 14:50:09 -0800

    Update Seiving_Prime.c

--- Seiving_Prime.c
+++ Seiving_Prime.c
@@ -29,60 +29,61 @@ int main (int argc, char *argv[])
           MPI_Finalize(); exit (1);
     }
     n = atoi(argv[1]);
-    low_value = 2 + BLOCK_LOW(id,p,n-1);
-    high_value = 2 + BLOCK_HIGH(id,p,n-1);
-    size = BLOCK_SIZE(id,p,n-1);
-    proc0_size = (n-1)/p;
-    if ((2 + proc0_size) < (int) sqrt((double) n)) {
-        if (!id) printf ("Too many processes\n");
-        MPI_Finalize();
-        exit (1);
-    }
-
-    marked = (char *) malloc (size);
-    if (marked == NULL) {
-        printf ("Cannot allocate enough memory\n");
-        MPI_Finalize();
-        exit (1);
-    }
-    for (i = 0; i < size; i++){
-        marked[i] = 0;
-    }
-    if (!id) index = 0;
-    prime = 2;
-    do{
-        if (prime * prime > low_value){
-            first = prime * prime - low_value;
-        }
-        else{
-            if (!(low_value % prime)){
-                first = 0;
-            }
-            else{
-                first = prime - (low_value % prime);
-            }
-        }
-        for (i = first; i < size; i += prime){
-            marked[i] = 1;
-        }
-        if (!id) {
-            while (marked[++index]);
-            prime = index + 2;
-        }
-        MPI_Bcast (&prime,  1, MPI_INT, 0, MPI_COMM_WORLD);
-    }while (prime * prime <= n);
-    count = 0;
-    for (i = 0; i < size; i++){
-        if (!marked[i]){
-            count++;
-        }
-    }
-    MPI_Reduce (&count, &global_count, 1, MPI_INT, MPI_SUM,0, MPI_COMM_WORLD);
-    elapsed_time += MPI_Wtime();
-    if (!id) {
-        printf ("Total number of primes: %d, Total time: %10.6f sec, Total nodes: 1\n",global_count,elapsed_time);
-        // printf ("Total elapsed time: %10.6f\n", elapsed_time);
-    }
+    printf("%c \t %c",argv[0],argv[1]);
+    // low_value = 2 + BLOCK_LOW(id,p,n-1);
+    // high_value = 2 + BLOCK_HIGH(id,p,n-1);
+    // size = BLOCK_SIZE(id,p,n-1);
+    // proc0_size = (n-1)/p;
+    // if ((2 + proc0_size) < (int) sqrt((double) n)) {
+    //     if (!id) printf ("Too many processes\n");
+    //     MPI_Finalize();
+    //     exit (1);
+    // }
+    // 
+    // marked = (char *) malloc (size);
+    // if (marked == NULL) {
+    //     printf ("Cannot allocate enough memory\n");
+    //     MPI_Finalize();
+    //     exit (1);
+    // }
+    // for (i = 0; i < size; i++){
+    //     marked[i] = 0;
+    // }
+    // if (!id) index = 0;
+    // prime = 2;
+    // do{
+    //     if (prime * prime > low_value){
+    //         first = prime * prime - low_value;
+    //     }
+    //     else{
+    //         if (!(low_value % prime)){
+    //             first = 0;
+    //         }
+    //         else{
+    //             first = prime - (low_value % prime);
+    //         }
+    //     }
+    //     for (i = first; i < size; i += prime){
+    //         marked[i] = 1;
+    //     }
+    //     if (!id) {
+    //         while (marked[++index]);
+    //         prime = index + 2;
+    //     }
+    //     MPI_Bcast (&prime,  1, MPI_INT, 0, MPI_COMM_WORLD);
+    // }while (prime * prime <= n);
+    // count = 0;
+    // for (i = 0; i < size; i++){
+    //     if (!marked[i]){
+    //         count++;
+    //     }
+    // }
+    // MPI_Reduce (&count, &global_count, 1, MPI_INT, MPI_SUM,0, MPI_COMM_WORLD);
+    // elapsed_time += MPI_Wtime();
+    // if (!id) {
+    //     printf ("Total number of primes: %d, Total time: %10.6f sec, Total nodes: 1\n",global_count,elapsed_time);
+    //     // printf ("Total elapsed time: %10.6f\n", elapsed_time);
+    // }
     MPI_Finalize ();
     return 0;
 }
