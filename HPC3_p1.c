dfc5a2b8939df6cacb9b58ec59b29fa1a8738412
Gautham25 <32765396+Gautham25@users.noreply.github.com>
2017-11-06 19:30:10 -0800

    Update HPC3_p1.c

--- HPC3_p1.c
+++ HPC3_p1.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
           if (!id) printf ("Command line: %s <m>\n", argv[0]);
           MPI_Finalize(); exit(1);
     }
-    n = atoi(argv[1]);
+    n = atoll(argv[1]);
     low_value = 3 + BLOCK_LOW(id,p,n-2) + BLOCK_LOW(id,p,n-2) % 2;
     high_value = 3 + BLOCK_HIGH(id,p,n-2) - BLOCK_HIGH(id,p,n-2) % 2;
     size = (high_value - low_value) / 2 +1;
