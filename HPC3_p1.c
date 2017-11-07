5e9ea8cc493ce1fc5fe3b92e52d61d7450df1f2b
Gautham25 <32765396+Gautham25@users.noreply.github.com>
2017-11-07 03:11:32 -0800

    Update HPC3_p1.c

--- HPC3_p1.c
+++ HPC3_p1.c
@@ -32,9 +32,8 @@ int main(int argc, char *argv[])
           if (!id) printf ("Command line: %s <m>\n", argv[0]);
           MPI_Finalize(); exit(1);
     }
-    n = atoll(argv[2]);
-    if(!id)
-        nodes = atoi(argv[1]);
+    n = atoll(argv[1]);
+
     low_value = 3 + BLOCK_LOW(id,p,n-2) + BLOCK_LOW(id,p,n-2) % 2;
     high_value = 3 + BLOCK_HIGH(id,p,n-2) - BLOCK_HIGH(id,p,n-2) % 2;
     size = (high_value - low_value) / 2 + 1;
@@ -97,7 +96,7 @@ int main(int argc, char *argv[])
     elapsed_time += MPI_Wtime();
     if (!id) {
         global_count++;
-        printf("Total number of primes: %llu, Total time: %10.6f sec, Total nodes: 10\n",global_count,elapsed_time);
+        printf("Total number of primes: %llu, Total time: %10.6f sec, Total nodes: %d\n",global_count,elapsed_time,argv[2]);
         printf("\nNUMBER %llu\n", n);
         // printf ("Total elapsed time: %10.6f\n", elapsed_time);
     }
