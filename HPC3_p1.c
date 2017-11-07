bb8fe4b5a3f800cfc7740baf34a0511f5b2ced2c
Gautham25 <32765396+Gautham25@users.noreply.github.com>
2017-11-07 01:21:54 -0800

    Update HPC3_p1.c

--- HPC3_p1.c
+++ HPC3_p1.c
@@ -21,6 +21,7 @@ int main(int argc, char *argv[])
     char *marked;
     unsigned long long int global_count;
     //variable declaration
+    nodes = atoi(argv[2]);
     MPI_Init(&argc, &argv);
     MPI_Barrier(MPI_COMM_WORLD);
     elapsed_time = -MPI_Wtime();
@@ -31,7 +32,7 @@ int main(int argc, char *argv[])
           MPI_Finalize(); exit(1);
     }
     n = atoll(argv[1]);
-    nodes = atoi(argv[2]);
+
     low_value = 3 + BLOCK_LOW(id,p,n-2) + BLOCK_LOW(id,p,n-2) % 2;
     high_value = 3 + BLOCK_HIGH(id,p,n-2) - BLOCK_HIGH(id,p,n-2) % 2;
     size = (high_value - low_value) / 2 +1;
@@ -98,4 +99,3 @@ int main(int argc, char *argv[])
     MPI_Finalize();
     return 0;
 }
-
