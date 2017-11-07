864c198be043afe2f0efd8b58e8ef0e793ab8e76
Gautham25 <32765396+Gautham25@users.noreply.github.com>
2017-11-07 03:04:43 -0800

    Update HPC3_p1.c

--- HPC3_p1.c
+++ HPC3_p1.c
@@ -33,6 +33,7 @@ int main(int argc, char *argv[])
           MPI_Finalize(); exit(1);
     }
     n = atoll(argv[1]);
+    nodes = atoi(argv[2]);
     low_value = 3 + BLOCK_LOW(id,p,n-2) + BLOCK_LOW(id,p,n-2) % 2;
     high_value = 3 + BLOCK_HIGH(id,p,n-2) - BLOCK_HIGH(id,p,n-2) % 2;
     size = (high_value - low_value) / 2 + 1;
