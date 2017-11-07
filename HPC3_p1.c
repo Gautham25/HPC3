99fb69a4da223ca77c3815958bd0f5707869629a
Gautham25 <32765396+Gautham25@users.noreply.github.com>
2017-11-07 03:00:12 -0800

    Update HPC3_p1.c

--- HPC3_p1.c
+++ HPC3_p1.c
@@ -22,17 +22,17 @@ int main(int argc, char *argv[])
     unsigned long long int global_count;
     //variable declaration
 
-    nodes = atoi(argv[2]);
+    //nodes = atoi(argv[2]);
     MPI_Init(&argc, &argv);
     MPI_Barrier(MPI_COMM_WORLD);
     elapsed_time = -MPI_Wtime();
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
     MPI_Comm_size(MPI_COMM_WORLD, &p);
-    if (argc != 3) {
+    if (argc != 2) {
           if (!id) printf ("Command line: %s <m>\n", argv[0]);
           MPI_Finalize(); exit(1);
     }
-    n = atol(argv[1]);
+    n = atoll(argv[1]);
     low_value = 3 + BLOCK_LOW(id,p,n-2) + BLOCK_LOW(id,p,n-2) % 2;
     high_value = 3 + BLOCK_HIGH(id,p,n-2) - BLOCK_HIGH(id,p,n-2) % 2;
     size = (high_value - low_value) / 2 + 1;
@@ -95,7 +95,7 @@ int main(int argc, char *argv[])
     elapsed_time += MPI_Wtime();
     if (!id) {
         global_count++;
-        printf("Total number of primes: %llu, Total time: %10.6f sec, Total nodes: %d\n",global_count,elapsed_time,nodes);
+        printf("Total number of primes: %llu, Total time: %10.6f sec, Total nodes: 10\n",global_count,elapsed_time);
         printf("\nNUMBER %llu\n", n);
         // printf ("Total elapsed time: %10.6f\n", elapsed_time);
     }
