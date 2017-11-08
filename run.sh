3e736912a8a61e6dcc18d987c27b28a968fb85d1
Gautham25 <manigauthamganesha@gmail.com>
2017-11-08 01:03:32 -0800

    HPC3 Comp

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ cd $PBS_O_WORKDIR
 
 mpicc -o HPC3_p1 HPC3_p1.c -lm
 mpicc -o HPC3_p2 HPC3_p2.c -lm
+mpicc -o HPC3_p3 HPC3_p3.c -lm
 
 jobid=$(qsub jobfile_part1_node1)
 Second=$(qsub -W depend=afterany:${jobid} jobfile_part1_node2)
@@ -15,4 +16,10 @@ Fourth=$(qsub -W depend=afterany:${Third} jobfile_part1_node8)
 PFirst=$(qsub -W depend=afterany:${Fourth} jobfile_part2_node1)
 PSecond=$(qsub -W depend=afterany:${PFirst} jobfile_part2_node2)
 PThird=$(qsub -W depend=afterany:${PSecond} jobfile_part2_node4)
-qsub -W depend=afterany:${PThird} jobfile_part2_node8
+PFourth=$(qsub -W depend=afterany:${PThird} jobfile_part2_node8)
+
+CFirst=$(qsub -W depend=afterany:${PFourth} jobfile_part3_node1)
+CSecond=$(qsub -W depend=afterany:${CFirst} jobfile_part3_node2)
+CThird=$(qsub -W depend=afterany:${CSecond} jobfile_part3_node4)
+qsub -W depend=afterany:${CThird} jobfile_part3_node8
+
