791d109b6228f3a2f697149320d554aef342f4c1
Gautham25 <manigauthamganesha@gmail.com>
2017-11-07 16:20:31 -0800

    P1P2C

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
 #1/bin/sh
 
-module purge
-module load gcc-4.6.2
-module load mvapich2-1.9a2/gnu-4.6.2
+PBS_O_WORKDIR=/home/$USER/HPC3
 
-mpicc -o HPC3_p1 HPC_p1.c -lm
-mpicc -o HPC3_p2 HPC_p2.c -lm
+cd $PBS_O_WORKDIR
 
-jobid=$(qsub jobfile_part1_node1|cut -d '.' -f 1)
-qsub -W depend=afterok:{jobid} jobfile_part1_node2
+mpicc -o HPC3_p1 HPC3_p1.c -lm
+mpicc -o HPC3_p2 HPC3_p2.c -lm
 
-jobid=$(qsub jobfile_part1_node2|cut -d '.' -f 1)
-qsub -W depend=afterok:{jobid} jobfile_part1_node4
+jobid=$(qsub jobfile_part1_node1)
+Second=$(qsub -W depend=afterany:${jobid} jobfile_part1_node2)
+Third=$(qsub -W depend=afterany:${Second} jobfile_part1_node4)
+Fourth=$(qsub -W depend=afterany:${Third} jobfile_part1_node8)
 
-jobid=$(qsub jobfile_part1_node4|cut -d '.' -f 1)
-qsub -W depend=afterok:{jobid} jobfile_part1_node8
-
+PFirst=$(qsub -W depend=afterany:${Fourth} jobfile_part2_node1)
+PSecond=$(qsub -W depend=afterany:${PFirst} jobfile_part2_node2)
+PThird=$(qsub -W depend=afterany:${PSecond} jobfile_part2_node4)
+qsub -W depend=afterany:${PThird} jobfile_part2_node8
