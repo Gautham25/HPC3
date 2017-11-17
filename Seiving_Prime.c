661b70ad3e56cc5adcd5c2bd04a4dcd3a5bcec23
Gautham25 <32765396+Gautham25@users.noreply.github.com>
2017-11-17 01:02:41 -0800

    Update Seiving_Prime.c

--- Seiving_Prime.c
+++ Seiving_Prime.c
@@ -20,7 +20,8 @@ int main(int argc, char *argv[])
     unsigned long long int n,k,low_value, high_value, size, proc0_size,i,prime,first;
     char *marked;
     unsigned long long int global_count;
-    //variable declaration
+    //variable declaration    
+    nodes = atoi(argv[2]);
     MPI_Init(&argc, &argv);
     MPI_Barrier(MPI_COMM_WORLD);
     elapsed_time = -MPI_Wtime();
@@ -31,7 +32,6 @@ int main(int argc, char *argv[])
           MPI_Finalize(); exit(1);
     }
     n = atoll(argv[1]);
-    nodes = atoi(argv[2]);
     low_value = 2 + BLOCK_LOW(id,p,n-1);
     high_value = 2 + BLOCK_HIGH(id,p,n-1);
     size = BLOCK_SIZE(id,p,n-1);
