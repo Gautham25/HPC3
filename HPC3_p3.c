4df34d87e9d2df781c05dce0a9488aa489c38779
Gautham25 <32765396+Gautham25@users.noreply.github.com>
2017-11-17 02:52:28 -0800

    Update HPC3_p3.c

--- HPC3_p3.c
+++ HPC3_p3.c
@@ -17,12 +17,12 @@ int main(int argc, char *argv[])
     //int *arrA = (int*)calloc(pow(10,10),sizeof(int));
     double elapsed_time;
     int id, index,p,count, nodes;
-    unsigned long long int n,k,low_value, high_value, size, proc0_size,i,prime,first;
+    int64_t n,k,low_value, high_value, size, proc0_size,i,prime,first;
     char *marked;
-    unsigned long long int global_count;
-    unsigned long long int localLow,localHigh,localSize,localFirst;
+    int64_t global_count;
+    int64_t localLow,localHigh,localSize,localFirst;
     char *localMarked;
-    unsigned long long int cacheSize, cStart, cEnd, cSize, cLow, cHigh;
+    int64_t cacheSize, cStart, cEnd, cSize, cLow, cHigh;
     cacheSize = 2000000;
     cStart=0;
     //variable declaration
@@ -30,14 +30,14 @@ int main(int argc, char *argv[])
     nodes = atoi(argv[2]);
     MPI_Init(&argc, &argv);
     MPI_Barrier(MPI_COMM_WORLD);
+    elapsed_time = -MPI_Wtime();
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
     MPI_Comm_size(MPI_COMM_WORLD, &p);
-    elapsed_time = -MPI_Wtime();
     if (argc != 3) {
           if (!id) printf ("Command line: %s <m>\n", argv[0]);
           MPI_Finalize(); exit(1);
     }
-    n = atoll(argv[1]);
+    n = atol(argv[1]);
     low_value = 3 + BLOCK_LOW(id,p,n-2) + BLOCK_LOW(id,p,n-2) % 2;
     high_value = 3 + BLOCK_HIGH(id,p,n-2) - BLOCK_HIGH(id,p,n-2) % 2;
     size = (high_value - low_value) / 2 + 1;
@@ -166,7 +166,7 @@ int main(int argc, char *argv[])
     elapsed_time += MPI_Wtime();
     if (!id) {
         global_count++;
-        printf("Total number of primes: %llu, Total time: %10.6f sec, Total nodes: %d\n",global_count,elapsed_time,nodes);
+        printf("Total number of primes: %ld, Total time: %10.6f sec, Total nodes: %d\n",global_count,elapsed_time,nodes);
         // printf ("Total elapsed time: %10.6f\n", elapsed_time);
     }
     MPI_Finalize();
