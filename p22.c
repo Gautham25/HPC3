08b552aa20b9e9c38a0bbe048e83ab0398e59136
Gautham25 <32765396+Gautham25@users.noreply.github.com>
2017-11-25 01:06:10 -0800

    Update p22.c

--- p22.c
+++ p22.c
@@ -73,6 +73,67 @@ int main(int argc, char *argv[])
     do{
         if (prime * prime > low_value){
             first = (prime * prime - low_value)/2;
+double elapsed_time;
+    int id, index,p,count, nodes;
+    unsigned long long int n,k,low_value, high_value, size, proc0_size,i,prime,first;
+    char *marked;
+    unsigned long long int global_count;
+    unsigned long long int low_proc0,high_proc0,size_proc0,first_proc0;
+    char *localMarked;
+    //variable declaration
+
+    nodes = atoi(argv[2]);
+    MPI_Init(&argc, &argv);
+    MPI_Barrier(MPI_COMM_WORLD);
+    elapsed_time = -MPI_Wtime();
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
+    if (argc != 3) {
+          if (!id) printf ("Command line: %s <m>\n", argv[0]);
+          MPI_Finalize(); exit(1);
+    }
+    n = atoll(argv[1]);
+    low_value = 3 + BLOCK_LOW(id,p,n-2) + BLOCK_LOW(id,p,n-2) % 2;
+    high_value = 3 + BLOCK_HIGH(id,p,n-2) - BLOCK_HIGH(id,p,n-2) % 2;
+    size = (high_value - low_value) / 2 + 1;
+    proc0_size = ((n-2)/(2*p));
+
+    low_proc0 = 3;
+    high_proc0 = (unsigned long long int)sqrt(n);
+    size_proc0 = (high_proc0 - low_proc0) / 2 + 1;
+    localMarked = (char*)malloc(size_proc0);
+
+    if (localMarked == NULL) {
+        printf("Cannot allocate memory to local array for seiving primes\n");
+        MPI_Finalize();
+        exit(1);
+    }
+
+    for(k=0;k<size_proc0;k++){
+        localMarked[k] = 0;
+    }
+
+    if ((3 + proc0_size) < (int) sqrt((double) n)) {
+        if (!id) printf ("Too many processes\n");
+        MPI_Finalize();
+        exit(1);
+    }
+
+    marked = (char *) malloc (size);
+    if (marked == NULL) {
+        printf("Cannot allocate enough memory\n");
+        MPI_Finalize();
+        exit(1);
+    }
+    for (i = 0; i < size; i++){
+        marked[i] = 0;
+    }
+
+    index = 0;
+    prime = 3;
+    do{
+        if (prime * prime > low_value){
+            first = (prime * prime - low_value)/2;
         }
         else{
             if (!(low_value % prime)){
@@ -96,6 +157,8 @@ int main(int argc, char *argv[])
             while (marked[++index]);
             prime = 2*index + 3;
         }
+        //if(p>1)
+            //MPI_Bcast(&prime,  1, MPI_INT, 0, MPI_COMM_WORLD);
         if(id){
             first_proc0 = (prime * prime - low_proc0)/2;
             for(k=first_proc0;k<size_proc0;k+=prime){
@@ -104,8 +167,7 @@ int main(int argc, char *argv[])
             while(localMarked[++index]);
             prime = 2 * index + 3;
         }
-    }while (prime * prime <= n);
-    count = 0;
+    }while (prime * prime <= n);    count = 0;
     for (i = 0; i < size; i++){
         if (!marked[i]){
             count++;
